841dfc8256f15fff1636009aa42985ecbd18a0a5
Asliddin <eliboyevasleshka@gmail.com>
2022-09-10 14:41:49 +0500

    Some texts added

--- src/Components/Home/Title.js
+++ src/Components/Home/Title.js
@@ -16,13 +16,7 @@ function Title() {
                     </div>
                     <div className='text'>
                       <p>
-                          Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                        when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-                        It has survived not only five centuries, but also the leap into electronic typesetting, 
-                        remaining essentially unchanged. It was popularised in the 1960s with the release of 
-                        Letraset sheets containing Lorem Ipsum passages, and more recently with desktop 
-                        publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                      «ZIYO FAYZ NON» oilaviy korxonasi 2017-yili O’zbekiston Respublikasi Samarqand viloyati Samarqand shahrida tashkil topdi. «ZIYO FAYZ NON» oilaviy korxonasi o’zining “ALHON” brendi ostida textile sanoatini ham yurutmoqda. Korxonamiz mahalliy qonunlar va qoidalarga rioya qilish bilan bir qatorda barcha xodimlarga qat’iy global axloq-qoidalarini qo‘llab keladi. "Alhon"tikuv sexi  sifatli mahsulot va ishonchli hamkor bo'lish bilan bir qatorda, o'z oldiga xodimlarini o‘qitishni va kuzatuv tizimlarini boshqarishni maqsad qilgan. Bundab tashqari xozirgi kunda adolatli va shaffof korporativ boshqaruvni amalga oshirib kelmoqda. Xozirgi kunda "Alhon" tikuv sexida 40 yaqin ishchi faoliyat yurutmoqda. Mahsulotlar turi esa 100 dan ortiq. "ALHON" o‘z iste’dodi va texnologiyalarini milliy va global jamiyatni rivojlantirishga hissa qo‘shadigan yuqori darajadagi mahsulotlarni yaratishga bag‘ishlaydi.
                       </p>
                     </div>
                   </div>
